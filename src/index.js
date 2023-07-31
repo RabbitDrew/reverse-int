5ba43a82bcb37867ccd32ac0199e9e0b6042ddb7
RabbitDrew <Andrei.sp.93@gmail.com>
2023-08-01 00:54:20 +0300

    init repo reverse-int and reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n>0) {
+        return Number(n.toString().split('').reverse().join("")) 
+    } else {
+        return Number(n.toString().split('').slice(1).reverse().join("")) 
+    }
 }
